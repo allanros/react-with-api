4eac035897079fe1445bdb6f69790f40fa1208eb
Allan <allan.rosendo@hotmail.com>
2024-08-07 12:34:37 -0300

    adding modal form to project

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import { HeaderContainer, HeaderContent, NewTransactionsButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
 
 import logoImg from '../../assets/logo.svg'
+import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
   return (
@@ -8,7 +10,13 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <NewTransactionsButton>Nova transação</NewTransactionsButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionsButton>Nova transação</NewTransactionsButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
